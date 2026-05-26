564af70efdee8829eeefa50d31b5c3fc0d5647ba
Ruslan Issabekov <bad.rak202@gmail.com>
2026-05-26 15:05:43 +0500

    v0.19: fix BGR color swap - picamera2 uses BGR natively

--- src/camera.py
+++ src/camera.py
@@ -13,7 +13,7 @@ class Camera:
                 from picamera2 import Picamera2
                 self.picam2 = Picamera2()
                 config = self.picam2.create_video_configuration(
-                    main={"size": (FRAME_WIDTH, FRAME_HEIGHT), "format": "RGB888"}
+                    main={"size": (FRAME_WIDTH, FRAME_HEIGHT), "format": "BGR888"}
                 )
                 self.picam2.configure(config)
                 self.picam2.start()
@@ -41,7 +41,6 @@ class Camera:
     def read(self):
         if self.mode == "picamera2":
             frame = self.picam2.capture_array()
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             return True, frame
         else:
             return self.cap.read()
