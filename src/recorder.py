b1046dbf5d875bddba23708f0f108e05f16332cc
Ruslan Issabekov <bad.rak202@gmail.com>
2026-06-03 11:01:04 +0500

    v0.26: segment recording every 10s + os.sync() for USB safety

--- src/recorder.py
+++ src/recorder.py
@@ -14,9 +14,13 @@ class Recorder:
         self.recording = False
         self.last_motion_time = 0
         self.lock = threading.Lock()
+        self._base_ts = ""
+        self._segment = 0
+        self._frame_in_segment = 0
+        self.SEGMENT_FRAMES = FPS * 10
 
-    def _get_filename(self):
-        return f"{RECORDINGS_DIR}/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.mp4"
+    def _get_filename(self, seg):
+        return f"{RECORDINGS_DIR}/{self._base_ts}_{seg:03d}.mp4"
 
     def _cleanup_old(self):
         free_mb = shutil.disk_usage(RECORDINGS_DIR).free / (1024 * 1024)
@@ -36,7 +40,13 @@ class Recorder:
 
     def _start_recording(self):
         self._cleanup_old()
-        filename = self._get_filename()
+        self._base_ts = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        self._segment = 1
+        self._frame_in_segment = 0
+        self._open_writer()
+
+    def _open_writer(self):
+        filename = self._get_filename(self._segment)
         fourcc = cv2.VideoWriter_fourcc(*'avc1')
         self.writer = cv2.VideoWriter(filename, fourcc, FPS, (FRAME_WIDTH, FRAME_HEIGHT))
         if not self.writer.isOpened():
@@ -49,16 +59,26 @@ class Recorder:
         with self.lock:
             if self.recording and self.writer:
                 self.writer.write(frame)
+                self._frame_in_segment += 1
+                if self._frame_in_segment >= self.SEGMENT_FRAMES:
+                    self._close_writer()
+                    self._segment += 1
+                    self._frame_in_segment = 0
+                    self._open_writer()
 
     def check_timeout(self):
         with self.lock:
             if self.recording and (time.time() - self.last_motion_time) > MOTION_TIMEOUT:
                 self._stop_recording()
 
-    def _stop_recording(self):
+    def _close_writer(self):
         if self.writer:
             self.writer.release()
             self.writer = None
+            os.sync()
+
+    def _stop_recording(self):
+        self._close_writer()
         self.recording = False
         print("[REC] Stopped")
 
